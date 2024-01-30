d71d015f02f61bc737096fb4191b735aa2dd1ca8
Kevin Hogan <kchogan@pacbell.net>
2024-01-29 20:37:04 -0800

    added first pass input questions

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // Import and require mysql2
 const mysql = require('mysql2');
+const input = require("./lib/input.js");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -22,20 +23,7 @@ const db = mysql.createConnection(
   console.log(`Connected to the employees_db database.`)
 );
 
-// Query database
-db.query('SELECT * FROM department', function (err, results) {
-  console.log(results);
-});
-
-// Query database
-db.query('SELECT * FROM role', function (err, results) {
-    console.log(results);
-  });
-
-// Query database
-db.query('SELECT * FROM employee', function (err, results) {
-    console.log(results);
-  });
+input.askQuestions();
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
