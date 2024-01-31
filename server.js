e0e8d32421c9fc73ce6d512add2320472e08673d
Kevin Hogan <kchogan@pacbell.net>
2024-01-30 21:28:09 -0800

    set arrays for future queries in prompt

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ const {
   updateEmpRole,
 } = require("./lib/input.js");
 const intro = require("./lib/intro.js");
-const { selectEmployees, selectRoles, selectDept } = require("./lib/queries.js");
+const {
+  selectEmployees,
+  selectRoles,
+  selectDept,
+} = require("./lib/queries.js");
 const { prompt } = require("inquirer");
-const { employeeTable, roleTable, deptTable } = require("./lib/tables.js")
+const { employeeTable, roleTable, deptTable } = require("./lib/tables.js");
 let conn;
 
 async function start() {
@@ -22,16 +26,20 @@ async function mainMenu() {
   const userChoice = await prompt(mainMenuQuestions);
   let quit = false;
   let deptData;
+  let empData;
+  let roleData;
   switch (userChoice.topMenu) {
     case "View All Employees":
       const employees = await conn.query(selectEmployees);
       employeeTable(employees[0]);
       break;
     case "Add Employee":
-      const empData = await prompt(addEmployee);
+      empData = await prompt(addEmployee);
       console.log(empData);
       break;
     case "Update Employee Role":
+      empData = await prompt(updateEmpRole);
+      console.log(empData);
       break;
     case "View All Roles":
       const roles = await conn.query(selectRoles);
@@ -43,11 +51,11 @@ async function mainMenu() {
       break;
     case "View All Departments":
       const departments = await conn.query(selectDept);
-      deptTable(departments[0])
+      deptTable(departments[0]);
       break;
     case "Add Department":
       deptData = await prompt(addDepartment);
-      console.log(deptData)
+      console.log(deptData);
       break;
     default:
       quit = true;
@@ -55,11 +63,10 @@ async function mainMenu() {
   }
 
   if (quit) {
-    process.exit()
+    process.exit();
   } else {
     mainMenu();
   }
-  
 }
 
 start();
