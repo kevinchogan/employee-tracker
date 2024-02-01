c481aab4a2ee6404fa6be964119a67e66564cb4f
Kevin Hogan <kchogan@pacbell.net>
2024-01-31 19:59:10 -0800

    updated comments

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const connection = require("./lib/connection.js");
-
+// Import inquirer questions
 const {
   mainMenuQuestions,
   addRoleQuestions,
@@ -7,34 +7,40 @@ const {
   addEmployeeQuestions,
   updateEmpRoleQuestions,
 } = require("./lib/input.js");
-
+// Import the intro text
 const intro = require("./lib/intro.js");
-
+// Import select queries
 const {
   selectEmployees,
   selectRoles,
   selectDept,
 } = require("./lib/queries.js");
-
+// Import functions for updating questions with latest data
 const {
   updateRoleArray,
   updateEmployeeArray,
   updateDeptArray,
 } = require("./lib/refresh.js");
 
-const { prompt } = require("inquirer");
-const { makeTable } = require("./lib/tables.js");
-const { getEmployeeId, getRoleId, getDeptId } = require("./lib/search.js");
-const { addEmployee, addRole, addDepartment } = require("./lib/add.js");
-const { updateEmpRole } = require("./lib/update.js");
+const { prompt } = require("inquirer"); //inquirer
+const { makeTable } = require("./lib/tables.js"); //displays tables
+const { getEmployeeId, getRoleId, getDeptId } = require("./lib/search.js"); //retrieves ids from db
+const { addEmployee, addRole, addDepartment } = require("./lib/add.js"); //adds to db
+const { updateEmpRole } = require("./lib/update.js"); //updates db
 let conn;
 
+/* === start ===
+Displays the app introduction and starts the Main Menu
+=== start ===*/
 async function start() {
   console.log(intro);
   conn = await connection;
   mainMenu();
 }
 
+/* === mainMenu ===
+Prompts the user with the Main Menu and handles the response
+=== mainMenu ===*/
 async function mainMenu() {
   const userChoice = await prompt(mainMenuQuestions);
   let quit = false;
@@ -45,11 +51,14 @@ async function mainMenu() {
   let roleId;
 
   switch (userChoice.topMenu) {
+    // View table of employees
     case "View All Employees":
       const employees = await conn.query(selectEmployees);
       makeTable(employees[0]);
       break;
+    // Add a new employee
     case "Add Employee":
+      // Update questions with latest roles and employees
       let updatedAddEmpQuestions = await updateRoleArray(
         conn,
         addEmployeeQuestions,
@@ -61,13 +70,16 @@ async function mainMenu() {
         3,
         true
       );
+      // Ask questions
       empData = await prompt(updatedAddEmpQuestions);
+      // Get the ids for role and manager
       roleId = await getRoleId(conn, empData.role);
       if (!empData.manager === "None") {
         managerId = await getEmployeeId(conn, empData.manager);
       } else {
         managerId = 0;
       }
+      // Add the employee
       await addEmployee(
         conn,
         empData.first_name,
@@ -76,7 +88,9 @@ async function mainMenu() {
         managerId
       );
       break;
+    // Update the role of an existing employee
     case "Update Employee Role":
+      // Update questions with latest employees and roles
       let updatedUpRoleQuestions = await updateEmployeeArray(
         conn,
         updateEmpRoleQuestions,
@@ -88,28 +102,39 @@ async function mainMenu() {
         updatedUpRoleQuestions,
         1
       );
+      // Ask questions
       empData = await prompt(updateEmpRoleQuestions);
+      // Get the role id
       roleId = await getRoleId(conn, empData.role);
+      // Update the role of the employee
       await updateEmpRole(conn, roleId, empData.name, empData.role);
       break;
+    // View table of all roles
     case "View All Roles":
       const roles = await conn.query(selectRoles);
       makeTable(roles[0]);
       break;
+    // Add a new role
     case "Add Role":
+      // Update questions with latest departments
       const updatedAddRoleQuestions = await updateDeptArray(
         conn,
         addRoleQuestions,
         2
       );
+      // Ask the questions
       roleData = await prompt(updatedAddRoleQuestions);
+      // Get the department id
       const departmentId = await getDeptId(conn, roleData.department);
+      // Add the role
       await addRole(conn, roleData.title, roleData.salary, departmentId);
       break;
+    // View table of all departments
     case "View All Departments":
       const departments = await conn.query(selectDept);
       makeTable(departments[0]);
       break;
+    // Add a new department
     case "Add Department":
       deptData = await prompt(addDepartmentQuestions);
       await addDepartment(conn, deptData.name);
